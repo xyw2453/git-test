387fbbb205e4d3e10aa3098fa45b458b095a2667
xyw <2453986438@qq.com>
2022-05-22 11:39:17 +0800

    hot-fix first commit

--- src/main/java/com.at.controller/MyController.java
+++ src/main/java/com.at.controller/MyController.java
@@ -5,5 +5,6 @@ public class MyController {
         System.out.println("initial");
         System.out.println("second");
         System.out.println("third");
+        System.out.println("hot-fix first commit");
     }
 }
