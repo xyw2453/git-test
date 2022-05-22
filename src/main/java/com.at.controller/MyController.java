cc7459651523237c25b1135ac0e23510794deebb
xyw2453 <62544717+xyw2453@users.noreply.github.com>
2022-05-22 14:30:36 +0800

    Update MyController.java

--- src/main/java/com.at.controller/MyController.java
+++ src/main/java/com.at.controller/MyController.java
@@ -9,5 +9,6 @@ public class MyController {
         System.out.println("four commit");
         System.out.println("hot-fix second commit");
         System.out.println("five commit");
+        System.out.println("remote first commit");
     }
 }
