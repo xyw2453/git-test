32c7d1abe73a008ee5f4314fa4d08a7503a6460d
xyw <2453986438@qq.com>
2022-05-22 09:55:35 +0800

    second commit

--- src/main/java/com.at.controller/MyController.java
+++ src/main/java/com.at.controller/MyController.java
@@ -3,5 +3,6 @@ package com.at.controller;
 public class MyController {
     public static void main(String[] args) {
         System.out.println("initial");
+        System.out.println("second");
     }
 }
